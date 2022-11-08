23beda82d3d453171fe1b4598de9a2697f1699fb
David Yang <dyang57@ucmerced.edu>
2022-11-08 11:01:28 -0800

    SQL queries added

--- NetflixDb.sql
+++ NetflixDb.sql
@@ -203,14 +203,6 @@ INSERT INTO Actors VALUES('Paulina Gaitán', 66);
 INSERT INTO Actors VALUES('Damián Alcázar', 67);
 INSERT INTO Actors VALUES('Francisco Denis', 68);
 
-
-
-
-
-
-
-
-
 --INSERT INTO MovieActors VALUES(movieid, actorid)
 INSERT INTO MovieActors VALUES(1, 1);--Forrest Gump
 INSERT INTO MovieActors VALUES(1, 2);
@@ -300,46 +292,65 @@ INSERT INTO Watchlist VALUES('pumagod', 2, NULL);
 INSERT INTO Watchlist VALUES('pumagod', 4, NULL);
 
 
-
-
-
-
-
-
-
-
 --SQL Statements
 
 --What Movies are comedies
-Select m_title
-From Movies
-Where m_genre LIKE '%Comedy%';
+-- Select m_title
+-- From Movies
+-- Where m_genre LIKE '%Comedy%';
 
 --What Actors are in Shrek 2
-Select a_actorname
-FROM Actors, MovieActors, Movies
-WHERE a_actorid = ma_actorid
-AND m_movieid = ma_movieid
-AND m_movieid = 2;
+-- Select a_actorname
+-- FROM Actors, MovieActors, Movies
+-- WHERE a_actorid = ma_actorid
+-- AND m_movieid = ma_movieid
+-- AND m_movieid = 2;
 
 --What movies does Tom Hanks star in
-Select m_title
-From Movies, Actors, MovieActors
-WHERE a_actorid = ma_actorid
-AND m_movieid = ma_movieid
-AND a_actorname = 'Tom Hanks';
-
+-- Select m_title
+-- From Movies, Actors, MovieActors
+-- WHERE a_actorid = ma_actorid
+-- AND m_movieid = ma_movieid
+-- AND a_actorname = 'Tom Hanks';
 
 --What shows has user 'Kidy101' added to their watchlist
-Select s_title
-From Shows, User, Watchlist
-WHERE s_showid = w_showid
-AND u_username = w_username
-AND u_username = 'Kidy101';
+-- Select s_title
+-- From Shows, User, Watchlist
+-- WHERE s_showid = w_showid
+-- AND u_username = w_username
+-- AND u_username = 'Kidy101';
 
 --What Movies has user 'pumagod' added to their watchlist
-Select m_title
-From Movies, User, Watchlist
+-- Select m_title
+-- From Movies, User, Watchlist
+-- WHERE m_movieid = w_movieid
+-- AND u_username = w_username
+-- AND u_username = 'pumagod';
+
+--What shows have more than 7 actors?
+-- SELECT DISTINCT s_title
+-- FROM Shows, Actors, ShowActors
+-- WHERE s_showid = sa_showid
+-- AND sa_actorid = a_actorid
+-- GROUP BY s_title
+-- HAVING COUNT(sa_actorid) > 6;
+
+--What movies has user 'pumagod' added to their watchlist that have been released before the year 2005?
+-- SELECT m_title
+-- FROM Movies, User, Watchlist
+-- WHERE m_movieid = w_movieid
+-- AND u_username = w_username
+-- AND u_username = 'pumagod'
+-- AND m_releaseDate < '2005-01-01';
+
+--What movies and shows have been released after the year 2000,
+--have been added to Kidy101's watchlist, and have less than 9 actors?
+SELECT m_title, s_title
+FROM Movies, Shows, User, Watchlist, ShowActors, MovieActors
 WHERE m_movieid = w_movieid
 AND u_username = w_username
-AND u_username = 'pumagod';
+AND u_username = 'Kidy101'
+AND m_releaseDate > '1999-12-12'
+AND s_releaseDate > '1999-12-12'
+GROUP BY m_title, s_title
+HAVING COUNT(ma_actorid) <= 8 AND COUNT(sa_actorid) <= 8;
